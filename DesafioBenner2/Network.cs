110a8b4f4db2f6a99e31ac05ea9ef0b0a0a8ccca
valmo <valmorhj100>
2024-05-24 20:53:14 -0300

    Adicionar arquivos de projeto.

---
+++ DesafioBenner2/Network.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Network
+{
+    private List<int>[] ListaDeControle;
+
+    public Network(int n)
+    {
+        if (n <= 0)
+            throw new ArgumentException("O número de elementos deve ser um inteiro positivo.");
+
+        ListaDeControle = new List<int>[n];
+        for (int i = 0; i < n; i++)
+        {
+            ListaDeControle[i] = new List<int>();
+        }
+    }
+
+    public void connect(int x, int y)
+    {
+        ValidarInteiroNalista(x);
+        ValidarInteiroNalista(y);
+
+        if (!ListaDeControle[x].Contains(y))
+        {
+            ListaDeControle[x].Add(y);
+        }
+
+        if (!ListaDeControle[y].Contains(x))
+        {
+            ListaDeControle[y].Add(x);
+        }
+    }
+
+    public bool query(int x, int y)
+    {
+        ValidarInteiroNalista(x);
+        ValidarInteiroNalista(y);
+
+        bool[] visitado = new bool[ListaDeControle.Length];
+        return EstaConectado(x, y, visitado);
+    }
+
+    private bool EstaConectado(int x, int y, bool[] visitado)
+    {
+        if (x == y) return true;
+
+        visitado[x] = true;
+
+        foreach (int vizinho in ListaDeControle[x])
+        {
+            if (!visitado[vizinho] && EstaConectado(vizinho, y, visitado))
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private void ValidarInteiroNalista(int x)
+    {
+        if (x < 0 || x >= ListaDeControle.Length)
+            throw new ArgumentException($"Número inválido: {x}. O Número deve estar entre 0 e {ListaDeControle.Length - 1}.");
+    }
+}
---
+++ DesafioBenner2/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    static void  Main(string[] args)
+    {
+        try
+        {
+            Network network = new Network(5);
+
+            network.connect(0, 1);
+
+            network.connect(1, 2);
+
+            network.connect(2, 3);
+
+            Console.WriteLine(network.query(0, 3)); // Saída: true
+
+            Console.WriteLine(network.query(3, 4)); // Saída: false
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ocorreu um erro: {ex.Message}");
+        }
+    }
+}
